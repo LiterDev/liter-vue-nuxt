821d400474f40d4a6d26f6dda326a5cb1aa741b3
neo <neo@literproject>
2018-07-18 18:11:47 +0900

    layer change component making....

---
+++ components/RecipeReviewCardList.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div>
+      RecipeReviewCardList
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'recipeReviewCardList',
+    data: function () {
+      return {
+        cardList: 'RecipeReviewCardList'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,70 @@
 <template>
-  <div>
-    <nuxt/>
+  <div class="container">
+  <md-app md-mode="fixed">
+    <md-app-toolbar class="md-primary">
+      <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <span class="md-title">My Title</span>
+      <md-icon>search</md-icon>
+    </md-app-toolbar>
+
+    <md-app-drawer :md-active.sync="menuVisible">
+      <md-toolbar class="md-transparent" md-elevation="0"><nuxt-link to="/">LITER</nuxt-link></md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <md-icon>move_to_inbox</md-icon>
+          <nuxt-link to="/inbox" class="md-list-item-text">Inbox</nuxt-link>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>send</md-icon>
+          <span class="md-list-item-text">Sent Mail</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>delete</md-icon>
+          <span class="md-list-item-text">Trash</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>error</md-icon>
+          <span class="md-list-item-text">Spam</span>
+        </md-list-item>
+      </md-list>
+    </md-app-drawer>
+
+    <md-app-content style="min-height:400px;">
+      <nuxt/>
+    </md-app-content>
+  </md-app>
   </div>
 </template>
+<script>
+  export default {
+    data: () => ({
+      menuVisible: false
+    })
+  }
+</script>
+<style lang="scss" scoped>
+  .md-app {
+    max-height: 400px;
+    border: 1px solid rgba(#000, .12);
+  }
+
+  // Demo purposes only
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+
+  .md-title {
+    flex: 1;
+    text-align:center;
+  }
+</style>
 
 <style>
 html {
@@ -21,6 +83,9 @@ html {
   margin: 0;
 }
 
+.container {
+  min-height:700px;
+}
 .button--green {
   display: inline-block;
   border-radius: 4px;
